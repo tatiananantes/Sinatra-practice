d734b2785301965e610497a7a2f9c35aae536298
Matt Zimmer <zimmermja@gmail.com>
2021-11-01 16:53:58 +0000

    HTML transferred to erb file

--- app.rb
+++ app.rb
@@ -18,25 +18,5 @@ get '/raining' do
 end
 
 get '/cat' do
-  content_type "text/html"
-  '
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <title>Cats</title>
-
-      <style>
-        img {
-          border: dashed red;
-          margin: 20px;
-          padding-right: 40px;
-        }
-      </style>
-    </head>
-
-    <body>
-      <img src="https://i.imgur.com/jFaSxym.png" alt="A cat" width="500" height="600">
-    </body>
-  </html>
-  '
+  erb(:index)
 end
